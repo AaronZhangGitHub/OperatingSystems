631be722d26260096ed1a7c51d844d62dee4b272
Aaron Zhang <aaronzhang@Aarons-Air.attlocal.net>
2017-09-18 21:58:28 -0400

    added first program outline info

--- HW2/530shell.c
+++ HW2/530shell.c
@@ -1,3 +1,14 @@
 //Aaron Zhang
+// I certify that no unauthorized assistance has been received or
+//given in the completion of this work
+//
 
 #include <stdio.h>
+
+int main(){
+    //Print prompt % for user input
+    printf("%%");
+    //Take stdin from user
+    
+    return 0;
+}
