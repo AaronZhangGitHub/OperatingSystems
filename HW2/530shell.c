88371d167bd185826258b5a09173577021f5d1ab
Aaron Zhang <aaronzhang@dhcp-v880-14-00848.1Xwireless.unc.edu>
2017-09-19 11:37:11 -0400

    fork/exec not working, reading stdin works correctly

--- HW2/530shell.c
+++ HW2/530shell.c
@@ -6,12 +6,14 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
+
 int main(){
     while(1){
         //Print prompt % for user input
         printf("%% ");
         //Take stdin from user, read line until a newline
-        int charInputLimit = 5;
+        int charInputLimit = 15;
         char input[charInputLimit+1];//Accounts for /0 with fgets
         //Check if there is an error with returning the string paremeter
         if(fgets(input,charInputLimit+1,stdin)==NULL){
@@ -20,24 +22,61 @@ int main(){
                 return 0;
             }else{
                 fprintf(stderr, "\nError with taking in input\n");
-                exit(EXIT_FAILURE);
+                exit(1);
             }
         }
         //Check if string input contains a newline, if not print to stderr
         if(strchr(input,'\n')==NULL){
             //No newline, meaning that the input was greater than the character limit
             fprintf(stderr, "Input greater than character limit of %i.\n",charInputLimit);
-            exit(EXIT_FAILURE);
+            exit(1);
         }
-        //Fork process to child process
+        //make a copy of the input string
+        char * copyInput = malloc(strlen(input)+1);
+        strcpy(copyInput,input);
         
-        //Parse input
-        char *filePath;
+        //Arguments
         char *whitespaceSeperators =" \t\v\f\r";
-        filePath = strtok(input,whitespaceSeperators);
-        printf("%s",filePath);
-        char *token = strtok(NULL,whitespaceSeperators);
-        printf("%s",token);
+        int whitespaceSeperatorCount = 1;
+        char *token = strtok(input,whitespaceSeperators);
+        if(token==NULL){
+            whitespaceSeperatorCount = 0;
+        }
+        while(token!=NULL){
+            token = strtok(NULL,whitespaceSeperators);
+            whitespaceSeperatorCount++;
+        }
+        if(whitespaceSeperatorCount<2){
+            fprintf(stderr, "Proper input not given\n");
+            exit(1);
+        }
+        //how many arguments there really are, this does not encompass the file path
+        int loopCount = whitespaceSeperatorCount-2;
+        //array for arguments
+        char *argArray[whitespaceSeperatorCount-1];
+        argArray[whitespaceSeperatorCount-2]=NULL; //Array of pointers must be terminated by null pointer
+        //get the file path
+        char *filePath= strtok(copyInput,whitespaceSeperators);
+        char *tokena;
+        for(int i=0;i<loopCount;i++){
+            argArray[i] = strtok(NULL,whitespaceSeperators);
+        }
+        
+        //Fork process to child process
+        int childPID;
+        childPID = fork();
+        if(childPID<0){
+            fprintf(stderr, "\nError with forking child.\n");
+            exit(1);
+        }else if(childPID==0){
+            int error = execvp(filePath,argArray);
+            if(error<0){
+                fprintf(stderr, "\nError with calling exec.\n");
+                exit(1);
+            }
+        }
+
+        free(copyInput);
     }
     return 0;
 }
