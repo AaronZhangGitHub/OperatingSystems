45f7df5892d69c968670eddacf28c3d34cc1f87d
Aaron Zhang <aaronz@live.unc.edu>
2017-10-31 16:38:59 -0400

    finished working version of receiving from server

--- HW5/HW5client.c
+++ HW5/HW5client.c
@@ -10,7 +10,8 @@
 int PORT_NUMBER;
 
 char * getInput(int charInputLimit);
-void sendInput(Socket *connect_socket);
+void sendInput(Socket connect_socket);
+void receiveInput(Socket connect_socket);
 
 int main (int argc, const char * argv[]) {
     Socket connect_socket;
@@ -25,22 +26,34 @@ int main (int argc, const char * argv[]) {
     printf("%s %i\n",dns_host_name,atoi(argv[2]));
     //connect to socket
     
-    connect_socket = Socket_new(dns_host_name, atoi(argv[2]));
+    connect_socket = Socket_new(dns_host_name, PORT_NUMBER);
     if (connect_socket < 0)
     {
         printf("Failed to connect to server\n");
         return (-1);
     }
-    //sendInput(connect_socket);
+    sendInput(connect_socket);
     
 }
 
-/*
-void sendInput(Socket *connect_socket){
+void receiveInput(Socket connect_socket){
     char c;
-    while((c = getChar())!= EOF){
-        printf(c);
+    while(c!=EOF && c!='\n'){
+        c = Socket_getc(connect_socket);
+        printf("%c",c);
     }
 }
 
-*/
+void sendInput(Socket connect_socket){
+    char c;
+    int count = 0;
+    while((c = getchar())!= EOF){
+        count++;
+        Socket_putc(c,connect_socket);
+        if(c=='\n'){
+            //wait for input
+            receiveInput(connect_socket);
+        }
+    }
+    Socket_putc(3,connect_socket);
+}
