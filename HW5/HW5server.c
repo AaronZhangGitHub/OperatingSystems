f14d562ac769a6ad721d3db95334b0be61b80cbe
Aaron Zhang <aaronz@live.unc.edu>
2017-10-31 18:51:11 -0400

    send to client from file not working

--- HW5/HW5server.c
+++ HW5/HW5server.c
@@ -5,6 +5,8 @@
 #include <unistd.h>
 #include <errno.h>
 #include "Socket.h"
+#include <stdlib.h>
+
 
 #define MAX_LINE 1024
 
@@ -12,7 +14,9 @@ ServerSocket welcomeSocket;
 Socket clientConnection;
 int PORT_NUMBER;
 
-int sendToClient(char *line_data,Socket clientConnection);
+int sendToClient(FILE *fp,Socket clientConnection);
+char * getCopiedString(char* input);
+int runShell(char *input, Socket clientConnection);
 
 int main (int argc, const char * argv[]) {
     PORT_NUMBER = atoi(argv[1]);
@@ -41,7 +45,8 @@ int main (int argc, const char * argv[]) {
                 if (c == '\n')
                 {
                     line_data[i] = c;
-                    sendToClient(line_data, clientConnection);
+                    //sendToClient(line_data, clientConnection);
+                    runShell(line_data, clientConnection);
                     memset(line_data, 0, MAX_LINE);
                     break;
                 }
@@ -51,25 +56,81 @@ int main (int argc, const char * argv[]) {
         }
     }
 }
+char * getCopiedString(char* input){
+    char * copyInput = malloc(strlen(input)+1);
+    strcpy(copyInput,input);
+    return copyInput;
+}
 
-int sendToClient(char *line_data,Socket clientConnection){
-    int count = strlen(line_data) + 1; /* include '\0' in count */
-    printf("%i",count);
-    char c;
-    char rc;
-    int i;
-    for (i = 0; i < count; i++)
-    {
-        c = line_data[i];
-        rc = Socket_putc(c, clientConnection);
-        if(c == '\n'){
-            return 0;
-        }
-        if (rc == EOF)
-        {
-            Socket_putc(EOF, clientConnection);
-            printf("Socket_putc EOF or error\n");
-            return 0;  /* assume socket EOF ends service for this client */
-        }
+int runShell(char *input, Socket clientConnection){
+    
+    //make a copy of the input string
+    char*copyInput = getCopiedString(input);
+    
+    //Arguments
+    char *whitespaceSeperators =" \t\v\f\r\n";
+    int whitespaceSeperatorCount = 1;
+    char *token = strtok(input,whitespaceSeperators);
+    if(token==NULL){
+        whitespaceSeperatorCount = 0;
     }
+    while(token!=NULL){
+        token = strtok(NULL,whitespaceSeperators);
+        whitespaceSeperatorCount++;
+    }
+    if(whitespaceSeperatorCount<2){
+        fprintf(stderr, "Proper input not given.\n");
+    }
+    //how many arguments there really are, this does not encompass the file path
+    int loopCount = whitespaceSeperatorCount-1;
+    //array for arguments
+    char *argArray[whitespaceSeperatorCount];
+    argArray[whitespaceSeperatorCount-1]=NULL; //Array of pointers must be terminated by null pointer
+    //get the file path
+    char *filePath= strtok(copyInput,whitespaceSeperators);
+    argArray[0] = filePath;
+    for(int i=1;i<loopCount;i++){
+        argArray[i] = strtok(NULL,whitespaceSeperators);
+    }
+    
+    //concat filename
+    int currentPID = getpid();
+    char *filename = malloc(7);
+    sprintf(filename, "tmp%d", currentPID);
+    
+    FILE *fp = freopen(filename, "w+", stdout);
+    
+    //Fork process to child process
+    int childPID;
+    childPID = fork();
+    if(childPID<0){
+        fprintf(stderr, "Error with forking child, %s.\n",strerror(errno));
+    }else if(childPID==0){
+        int error = execvp(filePath,argArray);
+        
+        sendToClient(fp, clientConnection);
+        if(error<0){
+            fprintf(stderr, "Error with calling exec, %s.\n",strerror(errno));
+            //kill child
+            exit(1);
+        }
+    }else{
+        pid_t terminated_pid;
+        do{
+            terminated_pid = wait(NULL);
+        }while(terminated_pid!=childPID);
+        fclose(fp);
+    }
+    //deallocate memory
+    free(copyInput);
+    return 0;
+}
+int sendToClient(FILE *fp,Socket clientConnection){
+    char ch;
+    while((ch = getc(fp)) != EOF)
+    {
+        Socket_putc(ch,clientConnection);
+    }
+    Socket_putc(EOF,clientConnection);
+    return 0;
 }
