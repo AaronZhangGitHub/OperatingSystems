662b9951a5b18ba0d21bd041d4cbf23059326189
Aaron Zhang <aaronz@live.unc.edu>
2017-11-11 19:43:02 -0500

    f

--- HW5/HW5client.c
+++ HW5/HW5client.c
@@ -1,4 +1,10 @@
-//Aaron Zhang
+
+//
+//
+//  Created by Aaron Zhang
+// I certify that no unauthorized assistance has been received or
+//given in the completion of this work
+//
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,6 +13,11 @@
 #include <errno.h>
 #include "Socket.h"
 
+/*
+ * main creates the connection to the given port and sends input to the port
+ * sendInput reads from stdin and sends input to the server when there is a newline
+ * receive input receives input from the server and writes to stdout
+ */
 int PORT_NUMBER;
 
 char * getInput(int charInputLimit);
@@ -23,9 +34,7 @@ int main (int argc, const char * argv[]) {
     char dns_host_name[200];
     strcpy(dns_host_name, argv[1]);
     PORT_NUMBER = atoi(argv[2]);
-    printf("%s %i\n",dns_host_name,atoi(argv[2]));
     //connect to socket
-    
     connect_socket = Socket_new(dns_host_name, PORT_NUMBER);
     if (connect_socket < 0)
     {
@@ -50,12 +59,14 @@ void receiveInput(Socket connect_socket){
 void sendInput(Socket connect_socket){
     char c;
     int count = 0;
+    printf("%% ");
     while((c = getchar())!= EOF){
         count++;
         Socket_putc(c,connect_socket);
         if(c=='\n'){
             //wait for input
             receiveInput(connect_socket);
+            printf("%% ");
         }
     }
     Socket_putc(3,connect_socket);
